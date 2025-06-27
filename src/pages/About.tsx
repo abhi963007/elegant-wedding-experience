9a31c98603090db022ad956c62d75c2f19800bf3
Abhiram <abhiramak963@gmail.com>
2025-06-27 19:37:48 +0530

    update

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -97,7 +97,7 @@ const About = () => {
             </p>
           </motion.div>
           
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-8 w-fit mx-auto">
             {[
               {
                 name: "Anandhu K S",
